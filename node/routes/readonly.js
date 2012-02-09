1337db40beef1a569fcde4f22b27313cce683bfe
booo <borgers@mi.fu-berlin.de>
2012-02-09 03:22:14 +0100

    simplify readonly route

--- node/routes/readonly.js
+++ node/routes/readonly.js
@@ -1,65 +1,41 @@
-var async = require('async');
-var ERR = require('async-stacktrace');
+module.exports = function(app) {
 
-module.exports = function(app)
-{
+    var hasPadAccess = require('./preconditions').hasPadAccess(app);
 
-  var hasPadAccess = require('./preconditions').hasPadAccess(app);
+    //serve read only pad
+    //FIXME use preconditions
+    app.get('/ro/:id', function(req, res, next) {
+        var html;
+        var pad;
 
-  //serve read only pad
-  app.get('/ro/:id', function(req, res)
-  {
-    var html;
-    var padId;
-    var pad;
-
-    async.series([
-      //translate the read only pad to a padId
-      function(callback)
-      {
-        app.readOnlyManager.getPadId(req.params.id, function(err, _padId)
-        {
-          if(ERR(err, callback)) return;
-
-          padId = _padId;
-
-          //we need that to tell hasPadAcess about the pad
-          req.params.pad = padId;
-
-          callback();
+        app.readOnlyManager.getPadId(req.params.id, function(error, padId) {
+            if(error) {
+                next(error);
+            } else if(padId == null){
+                res.send('404 - Not Found', 404);
+            } else {
+                //FIXME this
+                //we need that to tell hasPadAcess about the pad
+                req.params.pad = padId;
+                hasPadAccess(req, res, function()
+                {
+                    app.padManager.getPad(padId, function(error, pad) {
+                       if(error){
+                           next(error);
+                       } else {
+                          //render the html document
+                          app.exporthtml.getPadHTMLDocument(pad, null, false, function(error, html) {
+                              if (error) {
+                                  next(error);
+                              }
+                              else {
+                                  res.send(html);
+                              }
+                          });
+                       }
+                    });
+                });
+            }
         });
-      },
-      //render the html document
-      function(callback)
-      {
-        //return if the there is no padId
-        if(padId == null)
-        {
-          callback("notfound");
-          return;
-        }
-
-        hasPadAccess(req, res, function()
-        {
-          //render the html document
-          app.exporthtml.getPadHTMLDocument(padId, null, false, function(err, _html)
-          {
-            if(ERR(err, callback)) return;
-            html = _html;
-            callback();
-          });
-        });
-      }
-    ], function(err)
-    {
-      //throw any unexpected error
-      if(err && err != "notfound")
-        ERR(err);
-
-      if(err == "notfound")
-        res.send('404 - Not Found', 404);
-      else
-        res.send(html);
     });
-  });
 };
