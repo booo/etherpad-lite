eed9e3e8abe2f1c6bf82c3857d1e92a433f2c189
booo <borgers@mi.fu-berlin.de>
2012-02-11 01:29:48 +0100

    cosmetic changes

--- node/server.js
+++ node/server.js
@@ -29,7 +29,7 @@ var express = require('express');
 var log4js = require('log4js');
 var socketio = require('socket.io');
 
-var db = require('./db/DB');
+var DB = require('./db/DB');
 var minify = require('./utils/Minify');
 var parseSettings = require('./utils/Settings').parseSettings;
 
@@ -45,228 +45,225 @@ var TimesliderMessageHandler = require("./handler/TimesliderMessageHandler").Tim
 
 //try to get the git version
 var version = "";
-try
-{
-  var rootPath = path.normalize(__dirname + "/../");
-  var ref = fs.readFileSync(rootPath + ".git/HEAD", "utf-8");
-  var refPath = rootPath + ".git/" + ref.substring(5, ref.indexOf("\n"));
-  version = fs.readFileSync(refPath, "utf-8");
-  version = version.substring(0, 7);
-  console.log("Your Etherpad Lite git version is " + version);
-}
-catch(e)
-{
-  console.warn("Can't get git version for server header\n" + e.message);
+try {
+    var rootPath = path.normalize(__dirname + "/../");
+    var ref = fs.readFileSync(rootPath + ".git/HEAD", "utf-8");
+    var refPath = rootPath + ".git/" + ref.substring(5, ref.indexOf("\n"));
+    version = fs.readFileSync(refPath, "utf-8");
+    version = version.substring(0, 7);
+    console.log("Your Etherpad Lite git version is " + version);
+} catch(e) {
+    console.warn("Can't get git version for server header\n" + e.message);
 }
 
 console.log("Report bugs at https://github.com/Pita/etherpad-lite/issues");
 
 var serverName = "Etherpad-Lite " + version + " (http://j.mp/ep-lite)";
 
-var settings = parseSettings(__dirname + '/../settings.json');
-
-
-//set loglevel
-log4js.setGlobalLogLevel(settings.logLevel);
-
 async.waterfall([
-  //initalize the database
-  function (callback)
-  {
-    db.init(settings, callback);
-  },
-  //TOD rename dbInstance
-  //initalize the http server
-  function (dbInstance, callback) {
-    //create server
-    var app = express.createServer();
+    function(callback) {
+        var settings = parseSettings(__dirname + '/../settings.json');
+        callback(null, settings);
+    },
+    function(settings, callback) {
+        //set loglevel
+        log4js.setGlobalLogLevel(settings.logLevel);
+        callback(null, settings);
+    },
+    //initalize the database
+    function (settings, callback) {
+        DB.init(settings, function(error, db){
+            if(error) {
+                callback(error);
+            } else {
+                callback(null, settings, db);
+            }
+        });
+    },
+    //initalize the http server
+    function (settings, db, callback) {
+        //create server
+        var app = express.createServer();
 
-    app.settings = settings;
+        app.settings = settings;
 
-    app.use(function (req, res, next) {
-      res.header("Server", serverName);
-      next();
-    });
+        app.use(function (req, res, next) {
+            res.header("Server", serverName);
+            next();
+        });
 
-    //preconditions i.e. sanitize urls
-    require('./routes/preconditions')(app);
-
-
-    //load modules that needs a initalized db
-    app.readOnlyManager = new ReadOnlyManager(dbInstance);
-
-    app.authorManager = new AuthorManager(dbInstance);
-
-    app.padManager = new PadManager(settings, dbInstance, app.authorManager, app.readOnlyManager);
-
-    app.groupManager = new GroupManager(dbInstance, app.padManager, null);
-
-    app.sessionManager = new SessionManager(dbInstance, app.groupManager, app.authorManager);
-
-    app.securityManager = new SecurityManager(settings, dbInstance, app.authorManager, app.padManager, app.sessionManager);
-
-    app.exporthtml = require("./utils/ExportHtml");
-    app.exportHandler = require('./handler/ExportHandler');
-    app.importHandler = require('./handler/ImportHandler');
-
-    //app.apiHandler = require('./handler/APIHandler');
+        //preconditions i.e. sanitize urls
+        require('./routes/preconditions')(app);
 
 
-    //install logging
-    var httpLogger = log4js.getLogger("http");
-    app.configure(function()
-    {
-      // Activate http basic auth if it has been defined in settings.json
-      if(settings.httpAuth != null) app.use(basic_auth);
+        //load modules that needs a initalized db
+        app.readOnlyManager = new ReadOnlyManager(db);
 
-      // If the log level specified in the config file is WARN or ERROR the application server never starts listening to requests as reported in issue #158.
-      // Not installing the log4js connect logger when the log level has a higher severity than INFO since it would not log at that level anyway.
-      if (!(settings.loglevel === "WARN" || settings.loglevel == "ERROR")) {
-        app.use(log4js.connectLogger(httpLogger, { level: log4js.levels.INFO, format: ':status, :method :url'}));
-      }
-      app.use(express.cookieParser());
-    });
+        app.authorManager = new AuthorManager(db);
 
-    app.error(function(err, req, res, next){
-      res.send(500);
-      console.error(err.stack ? err.stack : err.toString());
-      gracefulShutdown();
-    });
+        app.padManager = new PadManager(settings, db, app.authorManager, app.readOnlyManager);
 
-    //serve static files
-    app.get('/static/js/require-kernel.js', function (req, res, next) {
-      res.header("Content-Type","application/javascript; charset: utf-8");
-      res.write(minify.requireDefinition());
-      res.end();
-    });
+        app.groupManager = new GroupManager(db, app.padManager, null);
 
-    //serve minified files
-    app.get('/minified/:filename', minify.minifyJS);
+        app.sessionManager = new SessionManager(db, app.groupManager, app.authorManager);
 
-    //checks for basic http auth
-    function basic_auth (req, res, next) {
-      if (req.headers.authorization && req.headers.authorization.search('Basic ') === 0) {
-        // fetch login and password
-        if (new Buffer(req.headers.authorization.split(' ')[1], 'base64').toString() == settings.httpAuth) {
-          next();
-          return;
+        app.securityManager = new SecurityManager(settings, db, app.authorManager, app.padManager, app.sessionManager);
+
+        app.exporthtml = require("./utils/ExportHtml");
+        app.exportHandler = require('./handler/ExportHandler');
+        app.importHandler = require('./handler/ImportHandler');
+
+        //app.apiHandler = require('./handler/APIHandler');
+
+
+        //install logging
+        var httpLogger = log4js.getLogger("http");
+        app.configure(function() {
+            // Activate http basic auth if it has been defined in settings.json
+            if(settings.httpAuth != null) {
+                app.use(basic_auth);
+            }
+
+            // If the log level specified in the config file is WARN or ERROR the application server never starts listening to requests as reported in issue #158.
+            // Not installing the log4js connect logger when the log level has a higher severity than INFO since it would not log at that level anyway.
+            if (!(settings.logLevel === "WARN" || settings.logLevel == "ERROR")) {
+                app.use(log4js.connectLogger(httpLogger, { level: log4js.levels.INFO, format: ':status, :method :url'}));
+            }
+            app.use(express.cookieParser());
+        });
+
+        app.error(function(err, req, res, next){
+            //TODO add global error prototype
+            res.send(500);
+            console.error(err.stack ? err.stack : err.toString());
+            gracefulShutdown();
+        });
+
+        //serve static files
+        app.get('/static/js/require-kernel.js', function (req, res, next) {
+            res.header("Content-Type","application/javascript; charset: utf-8");
+            res.write(minify.requireDefinition());
+            res.end();
+        });
+
+        //serve minified files
+        app.get('/minified/:filename', minify.minifyJS);
+
+        //checks for basic http auth
+        function basic_auth (req, res, next) {
+
+            if (req.headers.authorization && req.headers.authorization.search('Basic ') === 0) {
+                // fetch login and password
+                if (new Buffer(req.headers.authorization.split(' ')[1], 'base64').toString() == settings.httpAuth) {
+                    next();
+                    return;
+                }
+            }
+
+            res.header('WWW-Authenticate', 'Basic realm="Protected Area"');
+
+            if (req.headers.authorization) {
+                setTimeout(function () {
+                    res.send('Authentication required', 401);
+                }, 1000);
+            } else {
+                res.send('Authentication required', 401);
+            }
         }
-      }
 
-      res.header('WWW-Authenticate', 'Basic realm="Protected Area"');
-      if (req.headers.authorization) {
-        setTimeout(function () {
-          res.send('Authentication required', 401);
-        }, 1000);
-      } else {
-        res.send('Authentication required', 401);
-      }
+        require('./routes/readonly')(app);
+
+        require('./routes/import')(app);
+
+        require('./routes/export')(app);
+
+        //require('./routes/api')(app);
+
+        require('./routes/debug')(app);
+
+        require('./routes/static')(app);
+
+        //let the server listen
+        app.listen(settings.port, settings.ip);
+        console.log("Server is listening at " + settings.ip + ":" + settings.port);
+
+        var onShutdown = false;
+        var gracefulShutdown = function(err) {
+            if(err && err.stack) {
+                console.error(err.stack);
+            } else if(err) {
+                console.error(err);
+            }
+
+            //ensure there is only one graceful shutdown running
+            if(onShutdown) { return; }
+            onShutdown = true;
+
+            console.log("graceful shutdown...");
+
+            //stop the http server
+            app.close();
+
+            //do the db shutdown
+            db.doShutdown(function() {
+                console.log("db sucessfully closed.");
+
+                process.exit(0);
+            });
+
+            setTimeout(function() {
+                process.exit(1);
+            }, 3000);
+        };
+
+        //connect graceful shutdown with sigint and uncaughtexception
+        if(os.type().indexOf("Windows") == -1)
+        {
+            //sigint is so far not working on windows
+            //https://github.com/joyent/node/issues/1553
+            process.on('SIGINT', gracefulShutdown);
+        }
+
+        process.on('uncaughtException', gracefulShutdown);
+
+        //init socket.io and redirect all requests to the MessageHandler
+        var io = socketio.listen(app);
+
+        //this is only a workaround to ensure it works with all browers behind a proxy
+        //we should remove this when the new socket.io version is more stable
+        io.set('transports', ['xhr-polling']);
+
+        var socketIOLogger = log4js.getLogger("socket.io");
+        io.set('logger', {
+            debug: function (str) {
+                socketIOLogger.debug.apply(socketIOLogger, arguments);
+            },
+            info: function (str) {
+                socketIOLogger.info.apply(socketIOLogger, arguments);
+            },
+            warn: function (str) {
+                socketIOLogger.warn.apply(socketIOLogger, arguments);
+            },
+            error: function (str) {
+                socketIOLogger.error.apply(socketIOLogger, arguments);
+            }
+        });
+
+        //minify socket.io javascript
+        if(settings.minify) {
+            io.enable('browser client minification');
+        }
+
+        app.padMessageHandler = new PadMessageHandler(app.settings, app.padManager, app.authorManager, app.readOnlyManager, app.securityManager);
+
+        //Initalize the Socket.IO Router
+        //
+        app.socketIORouter = new SocketIORouter(app.securityManager);
+        app.timesliderMessageHandler = new TimesliderMessageHandler(app.settings, app.padManager, app.authorManager);
+        app.socketIORouter.setSocketIO(io);
+        app.socketIORouter.addComponent("pad", app.padMessageHandler);
+        app.socketIORouter.addComponent("timeslider", app.timesliderMessageHandler);
+
+        callback(null);
     }
-
-    require('./routes/readonly')(app);
-
-    require('./routes/import')(app);
-
-    require('./routes/export')(app);
-
-    //require('./routes/api')(app);
-
-    require('./routes/debug')(app);
-
-    require('./routes/static')(app);
-
-    //let the server listen
-    app.listen(settings.port, settings.ip);
-    console.log("Server is listening at " + settings.ip + ":" + settings.port);
-
-    var onShutdown = false;
-    var gracefulShutdown = function(err)
-    {
-      if(err && err.stack)
-      {
-        console.error(err.stack);
-      }
-      else if(err)
-      {
-        console.error(err);
-      }
-
-      //ensure there is only one graceful shutdown running
-      if(onShutdown) return;
-      onShutdown = true;
-
-      console.log("graceful shutdown...");
-
-      //stop the http server
-      app.close();
-
-      //do the db shutdown
-      dbInstance.doShutdown(function()
-      {
-        console.log("db sucessfully closed.");
-
-        process.exit(0);
-      });
-
-      setTimeout(function(){
-        process.exit(1);
-      }, 3000);
-    }
-
-    //connect graceful shutdown with sigint and uncaughtexception
-    if(os.type().indexOf("Windows") == -1)
-    {
-      //sigint is so far not working on windows
-      //https://github.com/joyent/node/issues/1553
-      process.on('SIGINT', gracefulShutdown);
-    }
-
-    process.on('uncaughtException', gracefulShutdown);
-
-    //init socket.io and redirect all requests to the MessageHandler
-    var io = socketio.listen(app);
-
-    //this is only a workaround to ensure it works with all browers behind a proxy
-    //we should remove this when the new socket.io version is more stable
-    io.set('transports', ['xhr-polling']);
-
-    var socketIOLogger = log4js.getLogger("socket.io");
-    io.set('logger', {
-      debug: function (str)
-      {
-        socketIOLogger.debug.apply(socketIOLogger, arguments);
-      },
-      info: function (str)
-      {
-        socketIOLogger.info.apply(socketIOLogger, arguments);
-      },
-      warn: function (str)
-      {
-        socketIOLogger.warn.apply(socketIOLogger, arguments);
-      },
-      error: function (str)
-      {
-        socketIOLogger.error.apply(socketIOLogger, arguments);
-      },
-    });
-
-    //minify socket.io javascript
-    if(settings.minify)
-      io.enable('browser client minification');
-
-
-    app.padMessageHandler = new PadMessageHandler(app.settings, app.padManager, app.authorManager, app.readOnlyManager, app.securityManager);
-
-
-
-    //Initalize the Socket.IO Router
-    //
-    app.socketIORouter = new SocketIORouter(app.securityManager);
-    app.timesliderMessageHandler = new TimesliderMessageHandler(app.settings, app.padManager, app.authorManager);
-    app.socketIORouter.setSocketIO(io);
-    app.socketIORouter.addComponent("pad", app.padMessageHandler);
-    app.socketIORouter.addComponent("timeslider", app.timesliderMessageHandler);
-
-    callback(null);
-  }
 ]);
